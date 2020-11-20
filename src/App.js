b48477831c5794eefab04f76e6634ef0b0dacc9d
aakash <arumugamaakash18@gmail.com>
2020-11-20 22:18:12 +0530

    Page-done

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Header from './components/Header'
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import HomeScreen from './components/HomeScreen';
 import PageContruction from './components/PageContruction';
-import Work from './components/Work';
 import Footer from './components/Footer';
 
 function App() {
@@ -12,10 +11,10 @@ function App() {
     <Router>
       <Header />
           <main className="app-main">
-            <Route path='/aakash' component={HomeScreen} exact />
-            <Route path='/aakash/work' component={PageContruction} />
-            <Route path='/aakash/about' component={PageContruction} />
-            <Route path='/aakash/contact' component={PageContruction} />
+            <Route path='/' component={HomeScreen} exact />
+            <Route path='/work' component={PageContruction} />
+            <Route path='/about' component={PageContruction} />
+            <Route path='/contact' component={PageContruction} />
           </main>
       <Footer />    
     </Router>
